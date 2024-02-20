068dc7d0c47ef9546c64ac4a24772a53280a5802
yuri-notfragile <vnm0774@naver.com>
2024-02-20 09:48:35 +0900

    first commit

---
+++ src/main/java/edu/fisa/lab/Step06CitestApplication.java
@@ -0,0 +1,13 @@
+package edu.fisa.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step06CitestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step06CitestApplication.class, args);
+	}
+
+}
